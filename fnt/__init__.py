2bda0a81ee9aecebb27bed5c1c35fc0243dfdb0c
A J Andrews <happydog.aja@gmail.com>
2025-05-23 14:16:26 +1200

    clean up init and add actual imports

--- fnt/__init__.py
+++ fnt/__init__.py
@@ -1,88 +1,52 @@
-"""
-Required Tables:
-    cmap (character to glyph mapping) - Created
-    head (font header) - Created, Parsable, Parsable
-    hhea (horizontal header) - Created, Parsable
-    hmtx (horizontal metrics) - Created, Parsable
-    maxp (maximum profile) - Created, Parsable
-    name (naming) - Created, Parsable (Missing Mac encodings)
-    OS/2 ( OS/2 and windows specific metrics) - Created, Parsable
-    post (PostScript) - Created, Parsable
+from .types import (
+    uint8,
+    int8,
+    uint16,
+    int16,
+    uint24,
+    int24,
+    uint32,
+    int32,
+    fixed,
+    UFWORD,
+    FWORD,
+    F2DOT14,
+    LONGDATETIME,
+    tag,
+    offset8,
+    offset16,
+    offset24,
+    offset32,
+    version16dot16,
+    table,
+)
+from .font import Font, ParseMethod
+from .file_font import FileFont
 
-Shared Tables:
-    cvt (control value)
-    fpgm (font program)
-    glyf (glyph outline) - Created
-    loca (index to location) - Created
-    gasp (grid-fitting and scan-conversion procedure)
+# TODO: tables
 
-    sbix (extended bitmaps)
-    EBSC (embedded bitmap scaling control)
-
-    avar (axis variation)
-    cvar (CVT variation)
-    fvar (font variation)
-    gvar (glyph variation)
-
-    kern (kerning)
-    meta (metadata)
-    hdmx (horizontal device metrics)
-    vmtx (vertical metrics)
-    vhea (vertical header)
-
-True Type Tables:
-    acnt (accent attachment)
-    ankr (anchor point)
-    bdat (bitmap data)
-    bhed (bitmap font header)
-    bloc (bitmap location)
-    bsln (baseline)
-    fdsc (font descriptor)
-    feat (layout feature)
-    fmtx (font metrics)
-    fond (font family compatibility)
-    just (justification)
-    kerx (extended kerning)
-    lcar (ligature caret)
-    ltag (language tag)
-    mort (metamorphosis) table (deprecated)
-    morx (extended metamorphosis)
-    opbd (optical bounds)
-    prop (properties)
-    trak (tracking)
-    xref (cross-reference)
-    Zapf (glyph reference)
-
-Open Type Tables:
-    SVG (scalar vector graphic glyphs)
-
-    CFF (Compact Font Format 1.0)
-    CFF2 (Compact Font Format 2.0)
-    VORG (Vertical Origin)
-    EBDT (Embedded bitmap data)
-    EBLC (Embedded bitmap location data)
-    CBDT (Color bitmap data)
-    CBLC (Color bitmap location data)
-
-    BASE (Baseline data)
-    GDEF (Glyph definition data)
-    GPOS (Glyph positioning data)
-    GSUB (Glyph substitution data)
-    JSTF (Justification data)
-    MATH (Math layout data)
-
-    HVAR (Horizontal metrics variations)
-    MVAR (Metrics variations)
-    STAT (Style attributes)
-    VVAR (Vertical metrics variations)
-
-    COLR (Color table)
-    CPAL (Color palette table)
-
-    DSIG (Digital Signature)
-    LTSH (Linear threshold map)
-    MERG (Merge)
-    STAT (Style attributes)
-    PCLT (PCL 5 data)
-    VDMX (Vertical device metrics)
-"""
+__all__ = (
+    "Font",
+    "FileFont",
+    "ParseMethod",
+    "uint8",
+    "int8",
+    "uint16",
+    "int16",
+    "uint24",
+    "int24",
+    "uint32",
+    "int32",
+    "fixed",
+    "UFWORD",
+    "FWORD",
+    "F2DOT14",
+    "LONGDATETIME",
+    "tag",
+    "offset8",
+    "offset16",
+    "offset24",
+    "offset32",
+    "version16dot16",
+    "table",
+)
