eba7f2586df3c2b9c634847160c061ddc8fe1994
DragonMoffon <happydog.aja@gmail.com>
2025-11-29 23:56:39 +1300

    delete unused legacy code

--- fnt/legacy/tables/prep.py
+++
@@ -1,6 +0,0 @@
-from fnt.types import Table, arrayEntry, propertyEntry, Array, uint8, uint32
-
-
-class prep(Table):
-    _length: uint32 = propertyEntry()
-    program: Array[uint8] = arrayEntry("_length")
