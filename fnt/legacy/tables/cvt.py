7cd32cdba754269e78aea3ccb00b53f7f1f393a5
DragonMoffon <happydog.aja@gmail.com>
2025-11-29 23:56:39 +1300

    delete unused legacy code

--- fnt/legacy/tables/cvt.py
+++
@@ -1,8 +0,0 @@
-from fnt.types import Table, dynamicEntry, propertyEntry, Array, FWORD, uint32
-
-
-class cvt(Table):
-    _length: uint32 = propertyEntry()
-    program: Array[FWORD] = dynamicEntry(
-        lambda l, t, b, o, s: t[l // 2].read(b, o + s), "length"
-    )
--- fnt/legacy/tables/fpgm.py
+++
@@ -1,6 +0,0 @@
-from fnt.types import Table, arrayEntry, propertyEntry, Array, uint8, uint32
-
-
-class fpgm(Table):
-    _length: uint32 = propertyEntry()
-    program: Array[uint8] = arrayEntry("_length")
