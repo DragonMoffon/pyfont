48b324a9e6597cfb3a32d5350465375a283ea5b4
DigiDuncan <digiduncan@gmail.com>
2025-12-03 16:52:05 -0500

    fix exceptions

--- fnt/exceptions.py
+++ fnt/exceptions.py
@@ -1,9 +1,7 @@
 class MissingTableError(KeyError):
-
     def __init__(self, table_name: str, *args):
-        KeyError.__init__(f"font does not contain the {table_name} table.", *args)
+        super().__init__(f"font does not contain the {table_name} table.", *args)
 
 class ParseError(ValueError):
-
-    def __init__(self, table_name: str, *args) -> None:
-        ValueError.__init__(f"failed to parse the {table_name} table", *args)
+    def __init__(self, table_name: str, *args):
+        super().__init__(f"failed to parse the {table_name} table", *args)
