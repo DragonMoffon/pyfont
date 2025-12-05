06b0cfd65ff42490f7edb97b583204fe3d5e463c
DragonMoffon <happydog.aja@gmail.com>
2025-12-05 17:53:00 +1300

    added viewer requirements as optional in pyproject.toml

--- viewer/__main__.py
+++ viewer/__main__.py
@@ -1,3 +1,10 @@
+try:
+    import textual
+    import unicodedataplus as unicodedata 
+except ImportError:
+    raise ImportError("failed to import required libraries make sure you have run `python -m pip install pyfont[viewer]`")
+
 from .run import run
 
-run()
+if __name__ == "__main__":
+    run()
