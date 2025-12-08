7c7aa936c9ab03238f5915102ef74e562cb16585
DragonMoffon <happydog.aja@gmail.com>
2025-12-08 13:08:21 +1300

    Collection can now create CollectionFonts which implement everything needed

--- fnt/collection.py
+++ fnt/collection.py
@@ -1,61 +1,71 @@
+from __future__ import annotations
 from pathlib import Path
 
-from .exceptions import MissingTableError
-from .types import uint32
+from .exceptions import MissingFontError, MissingTableError, ParseError
 from .font import Font, Reader, TableRecord, Table
 from .tables import TTCHeader
-from .parsing import parse_tcc_header
+from .parsing import parse_tcc_header, parse_table_directory, parsers
 
 
 
 # Collection Fonts have less control over their own bytes, and need to ask the Collection
 # for some data, this should have no impact on the end user
 class CollectionFont(Font):
+
+    def __init__(self, offset: int, source: Collection) -> None:
+        self._offset: int
+        self._source: Collection = source
+
+        table_directory = parse_table_directory(self, offset)
+        self._records: dict[str, TableRecord] = {
+            record.tableTag: record for record in table_directory.tableRecords
+        }
+        self._tables: dict[str, Table] = {"directory": table_directory}
     
     def validate_font(self) -> bool:
-        raise NotImplementedError()
+        return True # Font collections do not have the validation file fonts have.
 
     def get_record(self, name: str) -> TableRecord:
-        raise NotImplementedError()
-
-    def compute_checksum(self, start: int, length: int) -> uint32:
-        raise NotImplementedError()
-
-    def compute_table_checksum(self, name: str) -> uint32:
-        raise NotImplementedError()
-
-    def validate_checksum(self, name: str) -> bool:
-        if not self.has_table(name):
+        if name not in self._records:
             raise MissingTableError(name)
-        record = self.get_record(name)
-        checksum = self.compute_table_checksum(name)
-        
-        return checksum == record.checksum
+
+        return self._records[name]
 
     def get_table_names(self) -> tuple[str, ...]:
-        raise NotImplementedError()
+        return tuple(self._records.keys())
 
     def get_tables(self) -> tuple[Table, ...]:
-        raise NotImplementedError()
+        return tuple(self._tables.values())
 
     def get_table(self, name: str) -> Table | None:
-        raise NotImplementedError()
+        if name in self._tables:
+            return self._tables[name]
+
+        if name not in self._records:
+            raise MissingTableError(name)
+
+        record = self._records[name]
+        table = parsers[name](self, record)
+        if table is None: # type: ignore -- undefined parsers return None
+            raise ParseError(name)
+        self._tables[record.tableTag] = table
+
+        return table
 
     def has_table(self, name: str) -> bool:
-        raise NotImplementedError()
+        return name in self._records or name in self._tables
 
     def is_table_parsed(self, name: str) -> bool:
-        raise NotImplementedError()
+        return name in self._tables
     
     def seek(self, offset: int) -> None:
-        raise NotImplementedError()
+        self._source.seek(offset)
 
     def read(self, sz: int) -> bytes:
-        raise NotImplementedError()
+        return self._source.read(sz)
 
     def pointer(self) -> int:
-        raise NotImplementedError()
-
+        return self._source.pointer()
 
 
 class Collection(Reader):
@@ -67,10 +77,33 @@ class Collection(Reader):
         self._byte_offset: int = 0
 
         self._header = parse_tcc_header(self, 0)
+        self._fonts: dict[int, CollectionFont] = {}
 
     @property
     def header(self) -> TTCHeader:
         return self._header
+    
+    def get_font(self, idx: int) -> CollectionFont:
+        if idx in self._fonts:
+            return self._fonts[idx]
+
+        if self._header.numFonts <= idx:
+            raise MissingFontError(idx)
+        
+        font = CollectionFont(self._header.tableDirectoryOffsets[idx], self)
+        self._fonts[idx] = font
+        return font
+    
+    def get_fonts(self) -> tuple[CollectionFont, ...]:
+        for idx in range(self._header.numFonts):
+            self.get_font(idx)
+        return tuple(self._fonts.values())
+
+    def has_font(self, idx: int):
+        return 0 <= idx < self._header.numFonts
+
+    def is_font_created(self, idx: int):
+        return idx in self._fonts
 
     def seek(self, offset: int) -> None:
         self._byte_offset = offset
--- fnt/exceptions.py
+++ fnt/exceptions.py
@@ -1,6 +1,10 @@
+class MissingFontError(KeyError):
+    def __init__(self, idx: int, *args: object) -> None:
+        super().__init__(f"Collection does not contain {idx + 1} fonts", *args)
+
 class MissingTableError(KeyError):
     def __init__(self, table_name: str, *args):
-        super().__init__(f"font does not contain the {table_name} table.", *args)
+        super().__init__(f"font does not contain the {table_name} table", *args)
 
 class ParseError(ValueError):
     def __init__(self, table_name: str, *args):
